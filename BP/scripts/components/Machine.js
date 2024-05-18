9d6efd297ff9233e2042ffd42d6d8921bf1057cc
Redux <120261066+kumja1@users.noreply.github.com>
2024-05-18 17:18:02 -0400

    fix: issues occuring

--- BP/scripts/components/Machine.js
+++ BP/scripts/components/Machine.js
@@ -1,23 +1,21 @@
-import { world, system} from "@minecraft/server"
+import { world, system } from "@minecraft/server"
 import AllMachineBlocks from "../core/machines/AllMachineBlocks"
 import { MachineInstances } from "../core/machines/MachineInstances"
+import { cardinalToDirectionInt } from "../api/utilities/Direction"
 
 
 world.beforeEvents.worldInitialize.subscribe(({ blockTypeRegistry }) => {
     blockTypeRegistry.registerCustomComponent('cosmos:machine', {
-        beforeOnPlayerPlace({ player, permutationToPlace, block, dimension }) {
-            const machineType = AllMachineBlocks[permutationToPlace.type.id]
-            const direction = Math.round((player.getRotation().y + 180) / 90) * 90
-            const location = block.location;
-            const { x, y, z } = location
-            system.run(()=> {
-				dimension.setBlockType(location, 'minecraft:air')
-			})
-			dimension.runCommand(`summon ${machineType.tileEntity} ${x} ${y} ${z} ${direction}`)
-            MachineInstances.add(dimension, location, new machineType.class(block, dimension.getEntities({ location: block.center() })[0]))
-        },
-        onPlayerDestroy({ block, dimension }) {
-            MachineInstances.get(dimension, block.location).destroy();
+        onPlace({ block, dimension }) {
+            const machineType = AllMachineBlocks[block.typeId]
+            const entity = dimension.spawnEntity(machineType.tileEntity, { ...block.center(), y: block.y })
+            const location = block.location
+            dimension.setBlockType(location, 'minecraft:air')
+            MachineInstances.add(dimension, location, new machineType.class(block, entity))
         }
     })
+})
+
+world.afterEvents.entityHurt.subscribe(({ hurtEntity }) => {
+    if (hurtEntity && hurtEntity.typeId.startsWith("cosmos:")) MachineInstances.get(hurtEntity.dimension, hurtEntity.location)?.destroy()
 })
--- BP/scripts/core/machines/blocks/CoalGenerator.js
+++ BP/scripts/core/machines/blocks/CoalGenerator.js
@@ -1,42 +1,49 @@
-import { system } from "@minecraft/server";
+import { system, world } from "@minecraft/server";
 import { MachineBlockEntity } from "../base/MachineBlockEntity";
 
 export class CoalGenerator extends MachineBlockEntity {
-	constructor(block, entity) {
-		super(block, entity);
-		this.burnTime = this.entity.getDynamicProperty("burnTime") ?? 0
-		this.heat = this.entity.getDynamicProperty("heat") ?? 0
-		this.power = this.entity.getProperty("cosmos:power")
-		system.runInterval(() => this.generateHeat(), 10);
-	}
-	generateHeat() {
-		const fuelContainer = this.entity.getComponent('minecraft:inventory').container;
-		let fuelItem = fuelContainer.getItem(0);
-		if (!fuelItem || ["minecraft:coal", "minecraft:charcoal", "minecraft:coal_tileEntity"].includes(fuelItem.typeId)) return;
+    constructor(block, entity) {
+        super(block, entity);
+        this.fuelTypes = new Set(["minecraft:coal", "minecraft:charcoal", "minecraft:coal_block"]);
+        this.start();
+    }
 
-		//world.sendMessage(`bt ${Math.floor(burnTime /2)}s, heat ${heat}%, po ${power}gJ/t`)
+    start() {
+        this.runId = system.runInterval(() => {
+            if (!this.entity.isValid()) {
+                system.clearRun(this.runId);
+                return;
+            }
+            this.generateHeat();
+        }, 10);
+    }
 
-		fuelItem.decrementStack()
-		fuelContainer.setItem(0, fuelItem)
-		this.entity.setDynamicProperty("burnTime", fuelItem.typeId == 'minecraft:coalEntity' ? 3200 : 320)
+    generateHeat() {
+        const fuelContainer = this.entity.getComponent('minecraft:inventory').container;
+        let fuelItem = fuelContainer.getItem(0);
+        if (!fuelItem || !this.fuelTypes.has(fuelItem.typeId)) return;
 
-		if (this.burnTime > 0) {
-			this.entity.setDynamicProperty("burnTime", this.burnTime > 1 ? this.burnTime - 1 : 0)
-			this.entity.setDynamicProperty("heat", this.heat < 99 ? this.heat + 1 : 100)
-		}
-		if (this.burnTime == 0 && this.power == 0) {
-			this.entity.setDynamicProperty("heat", this.heat > 1 ? this.heat - 1 : 0)
-		}
-		if (this.burnTime != 0 && this.heat == 100 && this.burnTime % 3 == 0) {
-			this.entity.setProperty("cosmos:power", this.power < 119 ? this.power + 1 : 120)
-		}
-		if (this.burnTime == 0 && system.currentTick % 3 == 0) {
-			this.entity.setProperty("cosmos:power", this.power > 1 ? this.power - 1 : 0)
-		}
+        const isCoalBlock = fuelItem.typeId === 'minecraft:coal_block';
+        const burnTime = this.entity.getDynamicProperty("burnTime") || 0;
+        const heat = this.entity.getDynamicProperty("heat") || 0;
+        const power = this.entity.getProperty("cosmos:power");
+        fuelContainer.setItem(0, fuelItem.decrementStack());
+        this.entity.setDynamicProperty("burnTime", isCoalBlock ? 3200 : 320);
 
-		this.entity.nameTag = `bt ${Math.floor(this.burnTime / 2)}t, heat ${this.heat}%, po ${this.power}gJ/t`
-		//world.sendMessage(`bt ${Math.floor(bt /2)}s, heat ${he}%, po ${po}gJ/t`)
+        if (burnTime > 0) {
+            this.entity.setDynamicProperty("burnTime", burnTime > 1 ? burnTime - 1 : 0);
+            this.entity.setDynamicProperty("heat", heat < 99 ? heat + 1 : 100);
+        } else if (power === 0) {
+            this.entity.setDynamicProperty("heat", heat > 1 ? heat - 1 : 0);
+        }
 
-	}
+        if (burnTime !== 0 && heat === 100 && burnTime % 3 === 0) {
+            this.entity.setProperty("cosmos:power", power < 119 ? power + 1 : 120);
+        } else if (burnTime === 0 && system.currentTick % 3 === 0) {
+            this.entity.setProperty("cosmos:power", power > 1 ? power - 1 : 0);
+        }
+
+        this.entity.nameTag = `bt ${Math.floor(burnTime / 2)}t, heat ${heat}%, po ${power}gJ/t`;
+    }
 }
 
