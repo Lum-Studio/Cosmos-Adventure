96d2ef5bd9034fa8b7e83e70ffda50ae8421bc0d
Yasser4440 <85234823+Yasser4440@users.noreply.github.com>
2024-05-19 04:00:14 +0300

    Fixed the coal generator functionality

--- BP/scripts/core/machines/blocks/CoalGenerator.js
+++ BP/scripts/core/machines/blocks/CoalGenerator.js
@@ -15,35 +15,39 @@ export class CoalGenerator extends MachineBlockEntity {
                 return;
             }
             this.generateHeat();
-        }, 10);
+        });
     }
 
     generateHeat() {
         const fuelContainer = this.entity.getComponent('minecraft:inventory').container;
-        let fuelItem = fuelContainer.getItem(0);
-        if (!fuelItem || !this.fuelTypes.has(fuelItem.typeId)) return;
+        const fuelItem = fuelContainer.getItem(0);
 
-        const isCoalBlock = fuelItem.typeId === 'minecraft:coal_block';
         const burnTime = this.entity.getDynamicProperty("burnTime") || 0;
         const heat = this.entity.getDynamicProperty("heat") || 0;
         const power = this.entity.getProperty("cosmos:power");
-        fuelContainer.setItem(0, fuelItem.decrementStack());
-        this.entity.setDynamicProperty("burnTime", isCoalBlock ? 3200 : 320);
-
-        if (burnTime > 0) {
-            this.entity.setDynamicProperty("burnTime", burnTime > 1 ? burnTime - 1 : 0);
-            this.entity.setDynamicProperty("heat", heat < 99 ? heat + 1 : 100);
-        } else if (power === 0) {
+        const isCoalBlock = fuelItem?.typeId === 'minecraft:coal_block';
+		
+        if ( this.fuelTypes.has(fuelItem?.typeId) && burnTime == 0) {
+			fuelContainer.setItem(0, fuelItem.decrementStack())
+			this.entity.setDynamicProperty("burnTime", isCoalBlock ? 3200 : 320)
+		} else if (burnTime > 0) {
+			this.entity.setDynamicProperty("burnTime", burnTime > 1 ? burnTime - 1 : 0)
+			this.entity.setDynamicProperty("heat", heat < 99 ? heat + 1 : 100)
+		} else if (power === 0) {
             this.entity.setDynamicProperty("heat", heat > 1 ? heat - 1 : 0);
         }
 
         if (burnTime !== 0 && heat === 100 && burnTime % 3 === 0) {
             this.entity.setProperty("cosmos:power", power < 119 ? power + 1 : 120);
-        } else if (burnTime === 0 && system.currentTick % 3 === 0) {
+        }
+		if (burnTime === 0 && system.currentTick % 3 === 0) {
             this.entity.setProperty("cosmos:power", power > 1 ? power - 1 : 0);
         }
-
-        this.entity.nameTag = `bt ${Math.floor(burnTime / 2)}t, heat ${heat}%, po ${power}gJ/t`;
+		
+		const bt = this.entity.getDynamicProperty("burnTime") ?? 0
+		const he = this.entity.getDynamicProperty("heat") ?? 0
+		const po = this.entity.getProperty("cosmos:power")
+		this.entity.nameTag = `bt ${Math.floor(bt /2)}t, heat ${he}%, po ${po}gJ/t`
     }
 }
 
