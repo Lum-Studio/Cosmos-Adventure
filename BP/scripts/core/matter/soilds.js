c43bd217bd7700fc9d6570b2c5e1754860550162
Захаричек <174832353+Zaharichek@users.noreply.github.com>
2024-07-11 15:29:34 +0300

    fix arc lamp model in inventory

--- BP/scripts/core/matter/soilds.js
+++ BP/scripts/core/matter/soilds.js
@@ -40,6 +40,14 @@ world.beforeEvents.playerBreakBlock.subscribe((event) => {
 	}
 })
 
+world.beforeEvents.worldInitialize.subscribe(({ blockTypeRegistry }) => {
+	blockTypeRegistry.registerCustomComponent("cosmos:placed", {
+        beforeOnPlayerPlace(e){
+            e.permutationToPlace = e.permutationToPlace.withState("cosmos:placed", true);
+        },
+    });
+})
+
 system.runInterval(()=> {
 	world.getAllPlayers().forEach(player => {
 		const item = player.getComponent("minecraft:equippable").getEquipment("Mainhand")?.typeId
