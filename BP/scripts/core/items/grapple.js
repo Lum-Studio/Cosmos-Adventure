d17b47f3146d1588061e1b35c3f60aab964d6a1c
Захаричек <174832353+Zaharichek@users.noreply.github.com>
2024-07-17 22:46:51 +0300

    Update grapple.js

--- BP/scripts/core/items/grapple.js
+++ BP/scripts/core/items/grapple.js
@@ -1,15 +1,25 @@
 import {world} from "@minecraft/server";
 
-function grappleProjectileStop(source, location, projectile){
-	let generalProjectile = source.dimension.spawnEntity('cosmos:generalGrappleProjectile', source.location);
-    let direction = {x: location.x - source.location.x, y: location.y - source.location.y, z: location.z - source.location.z};
-    generalProjectile.getComponent('minecraft:rideable').addRider(source);
+function grappleProjectileStop(location, projectile){
+    let owner = world.getEntity(projectile.getDynamicProperty('owner'))
+	let generalProjectile = owner.dimension.spawnEntity('cosmos:generalGrappleProjectile', owner.location);
+    let direction = {x: location.x - owner.location.x, y: location.y - owner.location.y, z: location.z - owner.location.z};
+    generalProjectile.getComponent('minecraft:rideable').addRider(owner);
     generalProjectile.getComponent('minecraft:projectile').shoot(direction);
-    generalProjectile.setDynamicProperty('visualGrappleProjectile', projectile.id);
+    projectile.setDynamicProperty('generalGrappleProjectile', generalProjectile.id);
     //this function made for launching projectile with player to hook position|эта функция нужна для запуска снаряда с игроком к позиции крюка
 }
+world.beforeEvents.worldInitialize.subscribe(({itemComponentRegistry}) => {
+    itemComponentRegistry.registerCustomComponent("cosmos:grapple", {
+        onCompleteUse(data){
+            let visualProjectile = data.source.dimension.spawnEntity('cosmos:visuallGrappleProjectile', data.source.location);
+            visualProjectile.getComponent('minecraft:projectile').shoot(data.source.getViewDirection());
+            visualProjectile.setDynamicProperty('owner', data.source.id);
+        },
+    })
+})
 world.afterEvents.projectileHitBlock.subscribe((data) => {
     if(data.projectile.typeId != 'cosmos:visuallGrappleProjectile' && data.projectile.typeId != 'cosmos:generalGrappleProjectile') return;
-    if(data.projectile.typeId === 'cosmos:visuallGrappleProjectile') grappleProjectileStop(data.source, data.location, data.projectile);
-    if(data.projectile.typeId === 'cosmos:generalGrappleProjectile') try {world.getEntity(data.projectile.getDynamicProperty('visualGrappleProjectile')).triggerEvent("cosmos:despawn"), data.projectile.triggerEvent("cosmos:despawn")} catch(error) {null}
+    if(data.projectile.typeId === 'cosmos:visuallGrappleProjectile') grappleProjectileStop(data.location, data.projectile);
+    if(data.projectile.typeId === 'cosmos:generalGrappleProjectile') try {world.getEntity(data.projectile.getDynamicProperty('generalGrappleProjectile')).triggerEvent("cosmos:despawn"), data.projectile.triggerEvent("cosmos:despawn")} catch(error) {null}
 });
