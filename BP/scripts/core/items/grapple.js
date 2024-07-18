34825daae08f6c82ea648806517adb8d4423a735
Захаричек <174832353+Zaharichek@users.noreply.github.com>
2024-07-18 22:42:01 +0300

    Update grapple.js

--- BP/scripts/core/items/grapple.js
+++ BP/scripts/core/items/grapple.js
@@ -1,10 +1,10 @@
 import {world} from "@minecraft/server";
 
-function grappleProjectileStop(location, projectile){
+function grappleProjectileStop(projectile){
     let owner = world.getEntity(projectile.getDynamicProperty('owner'))
-	let generalProjectile = owner.dimension.spawnEntity('cosmos:gengrapple', owner.location);
-    let dirLenght = 1/Math.sqrt(((location.x - owner.location.x) * (location.x - owner.location.x)) + ((location.y - owner.location.y) * (location.y - owner.location.y)) + ((location.z - owner.location.z) * (location.z - owner.location.z)));
-    let direction = {x: (location.x - owner.location.x) * dirLenght, y: (location.y - owner.location.y) * dirLenght, z: (location.z - owner.location.z) * dirLenght};
+	let generalProjectile = owner.dimension.spawnEntity('cosmos:gengrapple', {x: owner.location.x, y: owner.location.y + 1, z: owner.location.z});
+    let dirLenght = 1/Math.sqrt(((projectile.location.x - owner.location.x) * (projectile.location.x - owner.location.x)) + ((projectile.location.y - owner.location.y) * (projectile.location.y - owner.location.y)) + ((projectile.location.z - owner.location.z) * (projectile.location.z - owner.location.z)));
+    let direction = {x: (projectile.location.x - owner.location.x) * dirLenght, y: (projectile.location.y - owner.location.y) * dirLenght, z: (projectile.location.z - owner.location.z) * dirLenght};
     generalProjectile.getComponent('minecraft:rideable').addRider(owner);
     generalProjectile.getComponent('minecraft:projectile').shoot(direction);
     projectile.setDynamicProperty('generalGrappleProjectile', generalProjectile.id);
@@ -21,7 +21,7 @@ world.beforeEvents.worldInitialize.subscribe(({itemComponentRegistry}) => {
 })
 world.afterEvents.projectileHitBlock.subscribe((data) => {
     if(data.projectile.typeId != 'cosmos:vgrapple' && data.projectile.typeId != 'cosmos:gengrapple') return;
-    if(data.projectile.typeId === 'cosmos:vgrapple') grappleProjectileStop(data.location, data.projectile);
+    if(data.projectile.typeId === 'cosmos:vgrapple') grappleProjectileStop(data.projectile);
     if(data.projectile.typeId === 'cosmos:gengrapple') {
         data.projectile.getComponent('minecraft:rideable').ejectRiders();
         try {world.getEntity(data.projectile.getDynamicProperty('generalGrappleProjectile')).triggerEvent("cosmos:despawn"), data.projectile.triggerEvent("cosmos:despawn")} catch(error) {null};
