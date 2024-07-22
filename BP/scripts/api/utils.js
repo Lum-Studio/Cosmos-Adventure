67d4807530ace16d4b07483ae40ff1d0be2ee017
Yasser4440 <85234823+Yasser4440@users.noreply.github.com>
2024-07-22 10:36:33 +0100

    Added get variables function

--- BP/scripts/api/utils.js
+++ BP/scripts/api/utils.js
@@ -1,17 +1,20 @@
 import machines from "../core/machines/AllMachineBlocks"
 
-function get_data(machine) { return machines[machine.typeId.replace('cosmos:machine:', '')] }
-function str(object) { return JSON.stringify(object) }
-function compare_lists(list1, list2) {
+export function get_data(machine) {
+	return machines[machine.typeId.replace('cosmos:machine:', '')]
+}
+
+export function str(object) { return JSON.stringify(object) }
+
+export function compare_lists(list1, list2) {
 	for (let i = 0; i < list1.length; i++) {
 		if (list1[i] != list2[i]) return false
 	} return true
 }
-export {
-    get_data,
-    str,
-    compare_lists
+export function get_vars(item, index, def = 0) {
+    return item ? + item.getLore()[index] : def
 }
+
 export const pickaxes = new Set([
 	"minecraft:wooden_pickaxe",
 	"minecraft:stone_pickaxe",
