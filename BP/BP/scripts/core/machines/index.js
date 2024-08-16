5f4c115549597eb875c0fcbe76d4baec786a3bf2
Yasser4440 <85234823+Yasser4440@users.noreply.github.com>
2024-08-16 14:58:22 +0100

    Delete the extra BP directory

--- BP/BP/scripts/core/machines/index.js
+++
@@ -1,6 +0,0 @@
-export * from './blocks/CoalGenerator'
-export * from './blocks/EnergyStorage'
-export * from './blocks/OxygenCollector'
-export * from './blocks/Compressor'
-export * from './blocks/CircuitFabricator'
-export * from './blocks/ElectricCompressor'
