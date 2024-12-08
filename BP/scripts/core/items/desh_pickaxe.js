2e6fb732fb4dd06a40c6a2eed9adafaf7ae99753
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2024-12-08 19:43:09 +0700

    did the script for the desh pickaxe

---
+++ BP/scripts/core/items/desh_pickaxe.js
@@ -0,0 +1,33 @@
+
+import { world, player, container, system, EnchantmentType, Player } from "@minecraft/server";
+
+
+class EnchantDeshPickaxe {
+    constructor(player) {
+        this.player = player;
+    }
+
+    enchant() {
+        let inv = this.player.getComponent("inventory").container
+        let item = inv.getItem(this.player.selectedSlotIndex);
+        
+        if (item && item.typeId === "cosmos:desh_pickaxe") {
+            let enchComp = item.getComponent('enchantable');
+            if (!enchComp) {
+                console.log("The selected item is not enchantable.");
+                return; // Exit if the item is not enchantable
+            }
+            const enchantment = { type: new EnchantmentType('silk_touch'), level: 1 };
+            enchComp.addEnchantment(enchantment);
+            inv.setItem(this.player.selectedSlotIndex, item);
+          }
+        }
+    }
+
+world.beforeEvents.worldInitialize.subscribe(({ blockComponentRegistry }) => {
+		blockComponentRegistry.registerCustomComponent('cosmos:enchant_desh', {
+			onPlayerDestroy({ block }) {
+				EnchantDeshPickaxe.enchant(block);
+			}
+		});
+	});
