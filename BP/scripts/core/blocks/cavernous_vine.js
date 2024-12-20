f9dd60de653681ce0e3691d850eabab94b01b11a
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2024-12-20 19:58:16 +0700

    idk

--- BP/scripts/core/blocks/cavernous_vine.js
+++ BP/scripts/core/blocks/cavernous_vine.js
@@ -1,4 +1,10 @@
 import { world, system, BlockPermutation } from "@minecraft/server"
+import {}
+function grab_player
+
+function poison_player
+
+class VineGrowth
 
 
 world.beforeEvents.worldInitialize.subscribe(({ blockComponentRegistry }) => {
