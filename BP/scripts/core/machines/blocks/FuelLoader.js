c7cb30ef643f20e54403b3c0e955e97193bd3fec
Захаричек <174832353+Zaharichek@users.noreply.github.com>
2024-12-30 12:48:36 +0300

    fuel loader functionality

--- BP/scripts/core/machines/blocks/FuelLoader.js
+++ BP/scripts/core/machines/blocks/FuelLoader.js
@@ -1,7 +1,26 @@
 import { system, ItemStack } from "@minecraft/server";
 import { MachineBlockEntity } from "../MachineBlockEntity.js";
-import { charge_from_battery, charge_from_machine } from "../../matter/electricity.js";
+import { charge_from_battery, charge_from_machine, location_of_side} from "../../matter/electricity.js";
 import { get_data, get_lore } from "../../../api/utils.js";
+
+function get_rockets(block){
+    let rockets = []
+    let pad_one = location_of_side(block, "front")
+    let pad_two = location_of_side(block, "back")
+    pad_one = block.dimension.getBlock({x: block.location.x + ((pad_one.x - block.location.x) * 2), y: block.location.y, z: block.location.z + ((pad_one.z - block.location.z) * 2)});
+    pad_two = block.dimension.getBlock({x: block.location.x + ((pad_two.x - block.location.x) * 2), y: block.location.y, z: block.location.z + ((pad_two.z - block.location.z) * 2)});
+    pad_one = (!pad_one.isAir && pad_one.typeId === "cosmos:rocket_launch_pad" && pad_one.permutation.getState("cosmos:center"))? pad_one:
+    undefined;
+    pad_two = (!pad_two.isAir && pad_two.typeId === "cosmos:rocket_launch_pad" && pad_two.permutation.getState("cosmos:center"))? pad_two:
+    undefined;
+    let rocket_one = (pad_one)? pad_one.dimension.getEntities({type: "cosmos:rocket_tier_1", location: pad_two.center(), maxDistance: 1}):
+    [];
+    let rocket_two = (pad_two)? pad_two.dimension.getEntities({type: "cosmos:rocket_tier_1", location: pad_two.center(), maxDistance: 1}):
+    [];
+    if(rocket_one.length > 0) rockets.push(rocket_one[0])
+    if(rocket_two.length > 0) rockets.push(rocket_two[0])
+    return rockets;
+}
 
 export default class extends MachineBlockEntity {
     constructor(block, entity) {
@@ -15,14 +34,33 @@ export default class extends MachineBlockEntity {
 		const input = container.getItem(0)
 		const data = get_data(this.entity)
 		const dimension = this.entity.dimension
-		
 		let energy = get_lore(container, data, "energy")
 		let fuel = get_lore(container, data, "fuel")
 		
 	    energy = charge_from_machine(this.entity, this.block, energy)
 		
 		energy = charge_from_battery(this.entity, energy, 1)
-			
+		
+		if (fuel + 1000 <= data.fuel_capacity && input?.typeId == "cosmos:fuel_bucket") {
+			container.setItem(0, new ItemStack('bucket'))
+			fuel += 1000
+		}
+		if(!stopped && energy > 120 && fuel > 2){
+		    let rockets = get_rockets(this.block)
+		    if(rockets.length > 0){
+		        rockets.forEach((rocket) =>{
+		            let fuel_level = rocket.getDynamicProperty("fuel_level")
+		            fuel_level = (fuel_level)? fuel_level:
+		            0;
+		            if(fuel_level < 1000){
+		                let level = Math.min(1000, fuel_level + 2)
+		            rocket.setDynamicProperty("fuel_level", level)
+		            fuel = Math.max(0, fuel - 2)
+		            }
+		        })
+		        
+		    }
+		}
 		const status =
 		energy == 0 ? "§4No Power" : 
 		fuel == 0 ? "§cNo Fuel" :
@@ -52,4 +90,4 @@ export default class extends MachineBlockEntity {
 		counter.setLore([''+energy, ''+fuel])
 		container.setItem(data.lore.slot, counter)
 	}
-			}
+}
