ece45dda60c789ff2d9a427408d6293a9f1b0d7e
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-03 17:04:31 +0700

    added syntatic flowery

--- BP/scripts/core/blocks/cavernous_vine.js
+++ BP/scripts/core/blocks/cavernous_vine.js
@@ -1,28 +1,35 @@
 import { world, system, player,  BlockPermutation, Player, Vector, Dimension } from "@minecraft/server"
 import { Vec3 } from "../../api/libraries/Vector";
-function grab_player() {
-    const players = world.getPlayers(); // Get all players
-    const entities = world.getEntity(); // Get all entities
 
-    // Apply knockback to each player
-    players.forEach(player => {
-        player.applyKnockback(0, 0, 0, 0.1); // Apply knockback to the player
-    });
+class CavernousVine {
+    constructor(world) {
+        this.world = world; 
+    }
 
-    // Apply impulse to each entity
-    entities.forEach(entity => {
-        if (!entity.isPlayer) { // Check if the entity is not a player
-            const vector = new Vec3(0, 0.1, 0); // Define a vector for impulse
-            entity.applyImpulse(vector); // Apply impulse to the entity
-        }
-    });
-}
+    grabPlayer() {
+        const players = this.world.getPlayers(); // Get all players
+        const entities = this.world.getEntities(); // Get all entities
 
-function poison_player() {
-    const players = world.getEntity();
-    e.forEach(e => {
-        e.addEffect("minecraft:poison",2);
-    })
+        // Apply knockback to each player
+        players.forEach(player => {
+            player.applyKnockback(0, 0, 0, 0.1); // Apply knockback to the player
+        });
+
+        // Apply impulse to each entity
+        entities.forEach(entity => {
+            if (!entity.isPlayer) { // Check if the entity is not a player
+                const vector = new Vec3(0, 0.1, 0); // Define a vector for impulse
+                entity.applyImpulse(vector); // Apply impulse to the entity
+            }
+        });
+    }
+
+    poisonPlayer() {
+        const players = this.world.getPlayers(); // Get all players
+        players.forEach(player => {
+            player.addEffect("minecraft:poison", 2); // Apply poison effect for 2 seconds
+        });
+    }
 }
 
 class VineGrowth
