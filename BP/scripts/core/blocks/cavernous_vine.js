8a26110f065d4696c3560c9a12714f10e6a730a4
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-03 18:42:57 +0700

    Update cavernous_vine.js

--- BP/scripts/core/blocks/cavernous_vine.js
+++ BP/scripts/core/blocks/cavernous_vine.js
@@ -3,15 +3,27 @@ import { Vec3 } from "../../api/libraries/Vector";
 
 class CavernousVine {
     constructor(world) {
-        this.world = world; 
+        this.world = world; e
     }
-
     grabPlayer() {
         const players = this.world.getPlayers(); // Get all players
         const entities = this.world.getEntities(); // Get all entities
+
+        // Apply knockback to each player
         players.forEach(player => {
             player.applyKnockback(0, 0, 0, 0.1); // Apply knockback to the player
         });
+
+        // Apply impulse to each entity
+        entities.forEach(entity => {
+            if (entity.typeId === 'minecraft:player') {
+            } else {
+                const vector = new Vec3(0, 0.1, 0); // Define a vector for impulse
+                entity.applyImpulse(vector); // Apply impulse to the entity
+            }
+        });
+    }
+
     poisonPlayer() {
         const players = this.world.getPlayers(); // Get all players
         players.forEach(player => {
