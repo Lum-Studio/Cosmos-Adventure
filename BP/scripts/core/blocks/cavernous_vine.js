d9ea739997aa3a24ab5b9f00bb46612af44fe34c
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-03 17:00:02 +0700

    I think this will works ?

--- BP/scripts/core/blocks/cavernous_vine.js
+++ BP/scripts/core/blocks/cavernous_vine.js
@@ -1,15 +1,29 @@
-import { world, system, player,  BlockPermutation, Player, Vector } from "@minecraft/server"
-import { delay } from "../../api/utils";
+import { world, system, player,  BlockPermutation, Player, Vector, Dimension } from "@minecraft/server"
+import { Vec3 } from "../../api/libraries/Vector";
+function grab_player() {
+    const players = world.getPlayers(); // Get all players
+    const entities = world.getEntity(); // Get all entities
 
-function grab_player(dimension) {
-    const players = world.getAllPlayers(); 
-    players.forEach(player => { 
-        player.applyKnockback(0, 0, 0, 0.1); 
+    // Apply knockback to each player
+    players.forEach(player => {
+        player.applyKnockback(0, 0, 0, 0.1); // Apply knockback to the player
+    });
+
+    // Apply impulse to each entity
+    entities.forEach(entity => {
+        if (!entity.isPlayer) { // Check if the entity is not a player
+            const vector = new Vec3(0, 0.1, 0); // Define a vector for impulse
+            entity.applyImpulse(vector); // Apply impulse to the entity
+        }
     });
 }
 
-
-function poison_player
+function poison_player() {
+    const players = world.getEntity();
+    e.forEach(e => {
+        e.addEffect("minecraft:poison",2);
+    })
+}
 
 class VineGrowth
 
