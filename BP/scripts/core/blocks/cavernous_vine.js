886ab3e1da59453e937bec124a4f56f22eabbd60
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-04 16:56:48 +0700

    fixing the vine

--- BP/scripts/core/blocks/cavernous_vine.js
+++ BP/scripts/core/blocks/cavernous_vine.js
@@ -1,62 +1,88 @@
 import { world, system, player,  BlockPermutation, Player, Vector, Dimension } from "@minecraft/server"
 import { Vec3 } from "../../api/libraries/Vector";
 
+import { world, system, TicksPerSecond, Block, Entity } from "@minecraft/server";
+import { Vec3 } from "../../api/libraries/Vector";
+
 class CavernousVine {
-    constructor(world) {
-        this.world = world; e
-    }
-    grabPlayer() {
-        const players = this.world.getPlayers(); // Get all players
-        const entities = this.world.getEntities(); // Get all entities
-
-        // Apply knockback to each player
-        players.forEach(player => {
-            player.applyKnockback(0, 0, 0, 0.1); // Apply knockback to the player
-        });
-
-        // Apply impulse to each entity
-        entities.forEach(entity => {
-            if (entity.typeId === 'minecraft:player') {
-            } else {
-                const vector = new Vec3(0, 0.1, 0); // Define a vector for impulse
-                entity.applyImpulse(vector); // Apply impulse to the entity
-            }
-        });
-    }
-
-    poisonPlayer(block) {
-        const block = block.location
-        const players = this.world.getPlayers(); // Get all players
-        players.forEach(player => {
-            player.addEffect("minecraft:poison", 40); // Apply poison effect for 2 seconds
-        });
-    }
-}
-    vineGrowth() ;{
-        let block = e.block
-        if (block.dimension.heightRange.min == block.y) return
-      
-        if (block.below()?.typeId == 'minecraft:air') {
-          block.below().setType(block.typeId == 'cosmos:cavernous_vine')
-        }
-      }
-//Register the Block and all its logics
-      onRegisterBlock() ;{
+    /**
+     * Register the Block and all its logics only once
+     * ```js
+     * CavernousVine.onRegisterBlock()
+     * ```
+     */
+    static onRegisterBlock() {
         world.beforeEvents.worldInitialize.subscribe(({ blockComponentRegistry }) => {
-            blockComponentRegistry.registerCustomComponent('cosmos:cavernous_vine', {
+            blockComponentRegistry.registerCustomComponent("cosmos:cavernous_vine", {
                 onTick({ block }) {
-                    const cavernousVine = new CavernousVine();
-                    cavernousVine.grabPlayer(block);
-                    cavernousVine.poisonPlayer(block);
+                    const cavernousVine = CavernousVine.get(block);
+                    cavernousVine.grabEntities();
                 },
                 onRandomTick({ block }) {
-                    const cavernousVine = new CavernousVine(); 
-                    cavernousVine.vineGrowth(block);
+                    const cavernousVine = CavernousVine.get(block);
+                    cavernousVine.vineGrowth();
                 }
             });
         });
+    };
+
+    /** @type {Map<string,{[y:number]:CavernousVine}>} */
+    static vineLocations = new Map();
+
+    /**
+     * @param {Block} block
+     * @returns {CavernousVine}
+     **/
+    static get(block) {
+        let key = `${block.x},${block.z}`;
+        let vines = this.vineLocations.get(key);
+        if (!vines) {
+            vines = this.vineLocations.set(key, {}).get(key);
+        }
+        return vines[block.y] ??= new CavernousVine(block, key);
     }
 
 
+    /**
+     * @private
+     * @param {Block} block
+     **/
+    constructor(block, key) {
+        this.block = block;
+        this.dimension = block.dimension;
+        this.getEntities = () => block.dimension.getEntitiesAtBlockLocation(block);
+        this.vineLocations_key = key;
+    }
+
+    grabEntities() {
+
+        // Apply impulse to each entity
+        for (const entity of this.getEntities()) {
+            if (!entity.hasComponent("movement")) continue;
+            if (entity.typeId === "minecraft:player") {
+                entity.applyKnockback(0, 0, 0, 0.1);
+            } else {
+                const vector = new Vec3(0, 0.1, 0); // Define a vector for impulse
+                entity.applyImpulse(vector); // Apply impulse to the entity
+            };
+            this.applyPoison(entity)
+        }
+    }
+    /**
+     * @param {Entity} entity 
+     */
+    applyPoison(entity) {
+        entity.addEffect("minecraft:poison", TicksPerSecond * 2); // Apply poison effect for 2 seconds
+    }
+
+    vineGrowth() {
+        const block = this.block;
+        if (block.dimension.heightRange.min == block.y) return
+
+        if (block.below()?.typeId == "minecraft:air") {
+            block.below().setType(block.typeId)
+        }
+    }
+};
+
 export default CavernousVine;
-    
