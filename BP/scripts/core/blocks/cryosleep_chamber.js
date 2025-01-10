38049c8091f6b020a5c00e2e993f8273c7ca9c52
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-10 19:20:36 +0700

    idt we have an use for that

---
+++ BP/scripts/core/blocks/cryosleep_chamber.js
@@ -0,0 +1,4 @@
+import { world, system, BlockPermutation } from "@minecraft/server"
+import { compare_position, get_entity, get_data, location_of_side} from "../matter/electricity.js"
+import machines from "../machines/AllMachineBlocks.js"
+
--- BP/scripts/planets/dimension/DimensionManager.js
+++
@@ -1,5 +0,0 @@
-import { Player, system, world } from "@minecraft/server"
-import { Mars } from "./Mars"
-import { Moon } from "./Moon"
-import { Venus } from "./Venus"
-
