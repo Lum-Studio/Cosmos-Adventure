f7ac84a687c14ab3c49a620bb5f5dbb87b3f9d3b
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-14 00:31:18 +0700

    this is useless

--- BP/scripts/planets/moon/EventHandlerMoon.js
+++ BP/scripts/planets/moon/EventHandlerMoon.js
@@ -23,7 +23,8 @@ world.afterEvents.entityDie.subscribe(({ deadEntity, damageSource: { damagingEnt
     if (deadEntity instanceof Player) {
         // Check if the player's location is in the Moon
         if (Moon.isOnLunar(deadEntity.location)) {
-            deadEntity.setSpawnPoint("cosmos:is_on_moon");
+            deadEntity.setDynamicProperty("cosmos:is_on_moon");
         }
     }
 });
+
