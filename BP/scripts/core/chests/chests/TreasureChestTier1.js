331db2687f1d134c3c0255b29bf810e5987d3908
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-27 20:46:03 +0700

    Ukraina super science in the work

--- BP/scripts/core/chests/chests/TreasureChestTier1.js
+++ BP/scripts/core/chests/chests/TreasureChestTier1.js
@@ -1,36 +1,50 @@
 import { world, system, MinecraftDimensionTypes } from "@minecraft/server";
 import { ChestUtils } from "ChestUtils";
+import { TaskQueue } from "../../../api/libraries/EndlessDB";
+
 world.beforeEvents.playerInteractWithEntity.subscribe((e) => {
     const { target: entity } = e;
-    if (entity.typeId !== "custom:chest_entity")
+    if (entity.typeId !== "cosmos:treasure_chest_tier1") {
         return;
+    }
     system.run(() => {
         new ChestUtils(entity).open();
     });
 });
+
 world.afterEvents.playerPlaceBlock.subscribe((e) => {
     const block = e.block;
     const { dimension, location: loc } = block;
     const fixedLocation = { x: loc.x + 0.5, y: loc.y, z: loc.z + 0.5 };
-    if (block.hasTag("custom_chest") || block.hasTag("custom_barrel")) {
-        const chestEntity = dimension.spawnEntity("custom:chest_entity", fixedLocation);
-        block.hasTag('custom_chest') ? chestEntity.nameTag = 'Chest' : chestEntity.nameTag = 'Barrel';
+    
+    if (block.hasTag("treasure_chest")) {
+        const chestEntity = dimension.spawnEntity("cosmos:treasure_chest_tier1", fixedLocation);
+        chestEntity.nameTag = 'treasure_chest';
     }
 });
+
+const taskQueue = new TaskQueue();
 system.runInterval(() => {
     const dimensions = [
         MinecraftDimensionTypes.overworld,
         MinecraftDimensionTypes.nether,
         MinecraftDimensionTypes.theEnd,
     ];
+
+    // Push tasks for each dimension
     for (const dimension of dimensions) {
-        const dim = world.getDimension(dimension);
-        for (const entity of dim.getEntities({ type: "custom:chest_entity" })) {
-            if (entity) {
-                const utils = new ChestUtils(entity);
-                utils.close();
-                utils.drop();
+        taskQueue.push(() => {
+            const dim = world.getDimension(dimension);
+            for (const entity of dim.getEntities({ type: "custom:chest_entity" })) {
+                if (entity) {
+                    const utils = new ChestUtils(entity);
+                    utils.close();
+                    utils.drop();
+                }
             }
-        }
+        });
     }
+
+    // Start processing tasks
+    taskQueue.run(5); // Adjust the run count based on performance needs
 });
