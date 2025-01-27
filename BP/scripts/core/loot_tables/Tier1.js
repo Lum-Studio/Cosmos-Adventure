f2691e042897060f486aca021271cadfb76d3ca7
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-27 22:52:33 +0700

    added the loot tables in js

--- BP/scripts/core/loot_tables/Tier1.js
+++ BP/scripts/core/loot_tables/Tier1.js
@@ -1,2 +1,28 @@
-export default new Map ([
-	
+export default new Map([
+    ["cosmos:cheese_curd", []],
+    ["minecraft:iron_ingot", ["minecraft:iron_ore", "minecraft:coal"]],
+    ["cosmos:food", [
+        "cosmos:canned_apple",
+        "cosmos:canned_carrot",
+        "cosmos:canned_melon",
+        "cosmos:canned_potato"
+    ]],
+    ["cosmos:oil_canister_partial", ["minecraft:iron_ingot"]],
+    ["minecraft:golden_apple", [
+        "minecraft:apple",
+        "minecraft:gold_ingot",
+        "minecraft:gold_ingot"
+    ]],
+    ["glowing_golden_apple", ["minecraft:golden_apple"]],
+    ["minecraft:redstone", ["minecraft:coal"]],
+    ["minecraft:record_blocks", []],
+    ["minecraft:record_far", []],
+    ["cosmos:steel_shovel", [
+        "cosmos:steel_ingot",
+        "minecraft:stick"
+    ]],
+    ["cosmos:item_basic_moon", ["cosmos:meteoric_iron_raw"]],
+    ["cosmos:meteoric_iron_raw", []],
+    ["cosmos:basic_item", []],
+    ["minecraft:book", ["minecraft:paper", "minecraft:leather"]],
+]);
---
+++ BP/scripts/core/loot_tables/Tier3.js
@@ -0,0 +1,29 @@
+export default new Map([
+    ["cosmos:food", [
+        "cosmos:canned_apple",
+        "cosmos:canned_carrot",
+        "cosmos:canned_melon",
+        "cosmos:canned_potato"
+    ]],
+    ["cosmos:desh_boots", []],
+    ["cosmos:desh_chestplate", []],
+    ["cosmos:desh_helmet", []],
+    ["cosmos:desh_leggings", []],
+    ["cosmos:oil_canister_partial", ["minecraft:iron_ingot"]],
+    ["cosmos:bucket_sulphuric_acid", []],
+    ["minecraft:golden_apple", [
+        "minecraft:apple",
+        "minecraft:gold_ingot",
+        "minecraft:gold_ingot"
+    ]],
+    ["glowing_golden_apple", ["minecraft:golden_apple"]],
+    ["minecraft:redstone", ["minecraft:coal"]],
+    ["minecraft:record_chirp", []],
+    ["minecraft:record_wait", []],
+    ["minecraft:record_ward", []],
+    ["cosmos:basic_item_venus", ["cosmos:meteoric_iron_raw"]],
+    ["minecraft:gold_nugget", ["minecraft:gold_ingot"]],
+    ["minecraft:book", ["minecraft:paper", "minecraft:leather"]],
+    ["minecraft:book", ["minecraft:paper", "minecraft:leather"]],
+    ["minecraft:book", ["minecraft:paper", "minecraft:leather"]],
+]);
