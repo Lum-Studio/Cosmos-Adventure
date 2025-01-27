cde63bc8f642e4e2d01e393a7fd9fe0d3d9c2dca
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-27 22:41:26 +0700

    delete some fancy

--- BP/scripts/planets/mars/TeleportTypeMars.js
+++
@@ -1,3 +0,0 @@
-import { Mars } from "../dimension/Mars";
-import { world, Player } from "@minecraft/server";
-
--- BP/scripts/planets/moon/TeleportTypeMoon.js
+++
@@ -1,3 +0,0 @@
-import { Moon } from "../dimension/Moon";
-import { world, Player } from "@minecraft/server";
-
