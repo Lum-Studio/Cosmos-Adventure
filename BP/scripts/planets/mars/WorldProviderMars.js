88912253fe9e96aa6172b01bc8ae58f87cad4299
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-30 22:46:24 +0700

    Update WorldProviderMars.js

--- BP/scripts/planets/mars/WorldProviderMars.js
+++ BP/scripts/planets/mars/WorldProviderMars.js
@@ -1,8 +1,8 @@
 import { Planet } from "../dimension/GalacticraftPlanets.js";
 import { system, world  } from "@minecraft/server";
+import { Gravity } from "../dimension/Gravity.js";
 
-
-
-let planet = Planet.getAll().find(planet => planet.isOnPlanet(player.location))
-
-world.afterEvents.playerDimensionChange.subscribe())
+let planet = Planet.get('mars')
+planet.getPlayers().forEach(player => {
+  new Gravity(player).set(planet.gravity)
+})
