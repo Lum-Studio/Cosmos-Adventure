1f388f356a5045678a21928758e188923cf8e222
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-01-30 19:43:04 +0700

    World Provider Planets

---
+++ BP/scripts/planets/dimension/GalacticraftPlanets.js
@@ -0,0 +1,88 @@
+import { world, Player } from "@minecraft/server";
+
+
+const the_end = world.getDimension('the_end');
+
+/**
+ * Class representing a generic Planet
+ */
+export class Planet {
+    /**
+     * Creates an instance of a Planet
+     * @param {Object} options - Options for the planet
+     * @param {string} options.name - Name of the planet
+     * @param {Object} options.range - Range of the planet
+     */
+    constructor({ name, range }) {
+        this.name = name;
+        this.range = range;
+        this.origin = {
+            x: (this.range.start.x + this.range.end.x) / 2,
+            z: (this.range.start.z + this.range.end.z) / 2
+        };
+    }
+
+    /**
+     * Checks whether a given location is on the planet
+     * @param {import("@minecraft/server").Vector3} location - Location to check
+     * @returns {boolean} Whether or not the location is on the planet
+     */
+    isOnPlanet(location) {
+        return (
+            this.range.start.x <= location.x && location.x <= this.range.end.x &&
+            this.range.start.z <= location.z && location.z <= this.range.end.z
+        );
+    }
+
+    /**
+     * Gets all entities in the End that match the EntityQueryOptions
+     * @param {EntityQueryOptions} entityQueryOptions - Query to use for search
+     * @returns {Entity[]} All entities matching the query
+     */
+    getEntities(entityQueryOptions) {
+        return the_end.getEntities(entityQueryOptions).filter(entity => 
+            this.isOnPlanet(entity.location)
+        );
+    }
+
+    /**
+     * Gets all players on the planet that match the EntityQueryOptions
+     * @param {EntityQueryOptions} entityQueryOptions - Query to use for search
+     * @returns {Player[]} All players matching the query
+     */
+    getPlayers(entityQueryOptions) {
+        return the_end.getPlayers(entityQueryOptions).filter(entity => 
+            this.isOnPlanet(entity.location)
+        );
+    }
+
+    /**
+     * Get the player location on the planet relative to the planet's origin
+     * @param {Player} player - The player object to get the location from
+     * @returns {import("@minecraft/server").Vector3} The player's position relative to the planet's origin
+     */
+    getPosition(player) {
+        return {
+            x: player.location.x - this.origin.x,
+            y: player.location.y, 
+            z: player.location.z - this.origin.z
+        };
+    }
+}
+
+
+/**
+ * Mars class extending the Planet class
+ */
+export class Mars extends Planet {
+    static range = {
+        start: { x: 50000, z: -50000 },
+        end: { x: 100000, z: -100000 }
+    };
+
+    constructor() {
+        super({ name: 'Mars', range: Mars.range });
+    }
+}
+
+export const MARS = the_end;
--- BP/scripts/planets/mars/WorldProviderMars.js
+++ BP/scripts/planets/mars/WorldProviderMars.js
@@ -1,68 +1,20 @@
 import {  Player, world  } from "@minecraft/server";
-import { Vec3, from, add } from "../../api/libraries/Vector.js"
+import { Planet } from "../dimension/GalacticraftPlanets.js";
 
 
 const the_end = world.getDimension('the_end');
+
 /**
- * Class containing methods relating to Mars
+ * Mars class extending the Planet class
  */
-export class Mars {
-    /**
-     * Range of blocks in the end that Mars takes up
-     */
+export class Mars extends Planet {
     static range = {
-        start: { x: -50000, z: 50000 },
+        start: { x: 50000, z: -50000 },
         end: { x: 100000, z: -100000 }
     };
 
-    /**
-     * Center block of Mars
-     */
-    static origin = {
-        x: (this.range.start.x + this.range.end.x) / 2,
-        z: (this.range.start.z + this.range.end.z) / 2
-    };
-
-    /**
-     * Checks whether a given location is on Mars
-     * @param {import("@minecraft/server").Vector3} location - Location to check
-     * @returns {boolean} Whether or not the location is on Mars
-     */
-    static isOnMars(location) {
-        return (
-            this.range.start.x <= location.x && location.x <= this.range.end.x &&
-            this.range.start.z <= location.z && location.z <= this.range.end.z
-        );
-    }
-
-    /**
-     * Gets all entities in the Moon that match the EntityQueryOptions
-     * @param {EntityQueryOptions} entityQueryOptions - Query to use for search
-     * @returns {Entity[]} All entities matching the query
-     */
-    static getEntities(entityQueryOptions) {
-        return the_end.getEntities(entityQueryOptions).filter(entity => 
-            this.isOnMars(entity.location)
-        );
-    }
-
-    /**
-     * Gets all players on Mars that match the EntityQueryOptions
-     * @param {EntityQueryOptions} entityQueryOptions - Query to use for search
-     * @returns {Player[]} All players matching the query
-     */
-    static getPlayers(entityQueryOptions) {
-        return the_end.getPlayers(entityQueryOptions).filter(entity => 
-            this.isOnMars(entity.location)
-        );
-    }
-    /**
-     * Get the player location on Mars
-     * @param {Player} player - The player object to get the location from
-     * @returns {Vec3} The adjusted position on Mars
-     */
-    static getPosition(player) {
-        return Vec3.add(player.location, Vec3(+750000, 0, -750000));
+    constructor() {
+        super({ name: 'Mars', range: Mars.range });
     }
 }
 
