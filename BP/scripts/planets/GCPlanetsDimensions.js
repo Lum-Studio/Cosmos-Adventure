51f9b97561f4c238970b8636a8334a242870fcea
Serty <timrahimov823@gmail.com>
2025-02-01 15:44:49 +0300

    Changed GCPlanetsDimensions.js

--- BP/scripts/planets/GCPlanetsDimensions.js
+++ BP/scripts/planets/GCPlanetsDimensions.js
@@ -1,14 +1,2 @@
-//Planet Registry
-import "./dimension/PlanetConfigs"
-
-//The Moon
-import "./moon/WorldProviderMoon"
-import "./moon/GlobalMoonEvents"
-
-//Mars
-//import "./mars/GlobalMartianEvents"
-//import "./mars/WorldProviderMars"
-
-//Venus
-//import "./venus/GlobalVenusEvents"
-//import "./venus/WorldProviderVenus"
+import "./dimension/PlanetConfigs.js"
+import "./dimension/GalacticraftPlanets.js"
