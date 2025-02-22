c4ecdce5675b9f0e269f2eb66ba5427c528b17d5
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-02-22 23:52:46 +0700

    Venera

--- BP/scripts/planets/dimension/PlanetConfigs.js
+++ BP/scripts/planets/dimension/PlanetConfigs.js
@@ -9,7 +9,7 @@ export const planetConfigs = [
     {
         id: 'venus',
         range: { start: { x: 50000, z: -100000 }, end: { x: 100000, z: -50000 } },
-        gravity: 9.8
+        gravity: 8.87
     },
     {
         id: 'moon',
@@ -28,5 +28,19 @@ planetConfigs.forEach(planet => {
     Planet.register(planet.id, {
         range: planet.range,
         gravity: planet.gravity
-    });
+    });    
 });
+
+// Adding Gravity
+system.runTimeout(() => {
+    for (let planet of Planet.getAll()) {
+        if (planet.id == 'venus') continue;
+        planet.events.onJoin('addGravity', ((event, player) => {
+            new Gravity(player).setTemp(planet.gravity)
+        }))
+
+        planet.events.onLeave('removeGravity', ((event, player) => {
+            new Gravity(player).setTemp(9.8)
+        }))
+    }
+})
