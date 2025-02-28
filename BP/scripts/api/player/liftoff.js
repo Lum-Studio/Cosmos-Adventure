7fc8df64d74b6ad0f990e7a3ff916cbdd1955263
Remember M9 <109746665+RememberM9@users.noreply.github.com>
2025-03-01 01:16:46 +0800

    Update liftoff.js

--- BP/scripts/api/player/liftoff.js
+++ BP/scripts/api/player/liftoff.js
@@ -95,25 +95,17 @@ function dismount(player) {
 function rocket_rotation(player, rocket) {
     let x = Math.round(player.inputInfo.getMovementVector().x);
     let y = Math.round(player.inputInfo.getMovementVector().y);
-    let rotationX = (x == 0 && y == 0) ?
-        rocket.getProperty("cosmos:rotation_x") :
-        (x == 0 && y == 1) ?
-            rocket.getProperty("cosmos:rotation_x") - 0.7 :
-            (x == 0 && y == -1) ?
-                rocket.getProperty("cosmos:rotation_x") + 0.7 :
-                rocket.getProperty("cosmos:rotation_x");
 
-    let rotationY = (x == 0 && y == 0) ?
-        rocket.getRotation().y :
-        (x == 1 && y == 0) ?
-            rocket.getRotation().y + 1 :
-            (x == -1 && y == 0) ?
-                rocket.getRotation().y - 1 :
-                rocket.getRotation().y;
-    rotationX = (rotationX > 180) ? 180 :
-        (rotationX < 0) ? 0 :
-            rotationX;
-    return [rotationX, rotationY]
+    let rotationX = rocket.getProperty("cosmos:rotation_x") +
+        ((x == 0 && y == 1) ? -0.7 :
+            (x == 0 && y == -1) ? 0.7 :
+                0);
+
+    return [
+        (rotationX > 180) ? 180 : (rotationX < 0) ? 0 : rotationX,
+        rocket.getRotation().y + ((x == 1 && y == 0) ? 1 : (x == -1 && y == 0) ? -1 : 0)
+    ]
+
 }
 
 function rocket_flight(rocket) {
