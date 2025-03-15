def81fc3a7a3e8ea0f088da7c23a3b41f6ea698a
ShadowEmpress001 <120316848+Senthemodder@users.noreply.github.com>
2025-03-15 21:18:10 +0700

    injected some useful methods into the Dimension class

--- BP/scripts/api/mixins.js
+++ BP/scripts/api/mixins.js
@@ -97,6 +97,24 @@ Container.prototype.add_ui_display = function (slot, text, damage) {
 	this.setItem(slot, button)
 }
 
+{
+    const Dimension = world.getDimension("overworld");
+    const dimensionProto = Object.getPrototypeOf(Dimension);
+    if (!dimensionProto.stopSound) {
+      /**
+       * Stops a sound at a specified location.
+       *
+       * @param {string} soundName - The name of the sound to stop.
+       * @param {{ x: number, y: number, z: number }} location - The location at which to stop the sound.
+       * @returns {} callback
+       */
+      dimensionProto.stopSound = function (soundName, location) {
+        return this.runCommand(
+          `execute positioned ${location.x} ${location.y} ${location.z} run stopsound @a ${soundName}`
+        );
+      };
+    }
+  }
 
 
 
