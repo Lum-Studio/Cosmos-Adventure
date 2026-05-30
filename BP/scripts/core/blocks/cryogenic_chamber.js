c0f5902c6d23801ba3e8e1ad60d2ad025cbf334c
Захаричек <174832353+Zaharichek@users.noreply.github.com>
2026-05-30 03:52:57 +0300

    Cryogenic Chamber

--- BP/scripts/core/blocks/cryogenic_chamber.js
+++ BP/scripts/core/blocks/cryogenic_chamber.js
@@ -1,34 +1,34 @@
-import { world, system, BlockPermutation, LinearSpline } from "@minecraft/server"
+import { world, system, BlockPermutation, CatmullRomSpline, AnimationOptions, EasingType, LinearSpline } from "@minecraft/server"
 
 
 export const cryogenic_chamber_component = {
     onPlayerInteract({player, block, dimension}){
         let states = block.permutation.getAllStates();
-        if(states["cosmos:is_occupied"]) return;
-
         let {x, y, z} = block.location;
         let pos = [ {x: x, y: y, z: z}, {x: x, y: y - 1, z: z}, {x: x, y: y - 2, z: z}
         ][states["cosmos:chamber"]];
 
+        let main_block = block.dimension.getBlock(pos);
+        if(main_block.permutation.getState("cosmos:is_occupied")) return;
+
         let time = player.getDynamicProperty("chamber_time");
 
         if(!time || system.currentTick > time){
-            
-            /*it works but it is so choppy
-            let degree = 90;
-            let cam = 180;
-            let cam_cycle = system.runInterval(() => {
-                degree += 5;
-                console.warn(degree)
-                cam += 5;
-                player.camera.setCamera("minecraft:free", 
-                    {location: {x: pos.x + 0.5 + Math.cos(degree/57.2957795147) * 5, y: pos.y + 2, z: pos.z + 0.5 + Math.sin(degree/57.2957795147) * 5}, rotation: {x: 0, y: cam}}
-                    );
-                if(degree >= 360){
-                    player.camera.clear();
-                    system.clearRun(cam_cycle)
-                }
-            });*/
+            let degree = {north: 270, south: 90, east: 0, west: 180}[states["minecraft:cardinal_direction"]];
+            let cam_degree = {north: 90, south: 270, east: 0, west: 180}[states["minecraft:cardinal_direction"]];
+
+            player.inputPermissions.setPermissionCategory(1, false);
+            player.inputPermissions.setPermissionCategory(2, false);
+            player.teleport({x: pos.x + 0.5, y: pos.y + 0.75, z: pos.z + 0.5}, {rotation: {x: 21, y: degree + 90}});
+            play_camera_animation(player, pos, degree, cam_degree);
+
+            main_block.setPermutation(main_block.permutation.withState("cosmos:is_occupied", true));
+            system.runTimeout(() => {
+                if(main_block.typeId == "cosmos:cryogenic_chamber") main_block.setPermutation(main_block.permutation.withState("cosmos:is_occupied", false));
+                player.inputPermissions.setPermissionCategory(1, true);
+                player.inputPermissions.setPermissionCategory(2, true);
+                player.camera.clear();
+            }, 72);
 
             let added_time = 0;
             let planet = player.getPlanet(); 
@@ -36,20 +36,22 @@ export const cryogenic_chamber_component = {
                 added_time = planet.time.day;
             }else added_time = 12000;
 
-            player.teleport({x: pos.x + 0.5, y: pos.y + 0.75, z: pos.z + 0.5});
-
             world.setAbsoluteTime(world.getAbsoluteTime() + added_time);
             player.setDynamicProperty("chamber_time", system.currentTick + 6000);
         }else{
-            world.sendMessage(`${Math.floor((time - system.currentTick)/20)}`);
+            world.sendMessage({"rawtext": [{"text": "I can't use this for another"},
+                {"text": ` ${Math.floor((time - system.currentTick)/20)} `},
+                {"translate": "seconds"}
+            ]});
         }
     },
 
     beforeOnPlayerPlace(event){
         if(!event.block.above().isAir || !event.block.above(2).isAir) event.cancel = true;
         else system.run(() => {
-                event.block.above().setPermutation(BlockPermutation.resolve("cosmos:cryogenic_chamber", {"cosmos:chamber": 1}));
-                event.block.above(2).setPermutation(BlockPermutation.resolve("cosmos:cryogenic_chamber", {"cosmos:chamber": 2}));
+                let direction = event.permutationToPlace.getState("minecraft:cardinal_direction")
+                event.block.above().setPermutation(BlockPermutation.resolve("cosmos:cryogenic_chamber", {"cosmos:chamber": 1, "minecraft:cardinal_direction": direction}));
+                event.block.above(2).setPermutation(BlockPermutation.resolve("cosmos:cryogenic_chamber", {"cosmos:chamber": 2, "minecraft:cardinal_direction": direction}));
             });
     },
     onPlayerBreak({brokenBlockPermutation: perm, block}) {
@@ -63,4 +65,54 @@ export const cryogenic_chamber_component = {
             block.dimension.getBlock(vector).setPermutation(BlockPermutation.resolve("air"))
         });
     },
+    onTick({block}){
+        if(block.permutation.getState("cosmos:chamber")) return;
+        let {x, y, z} = block.location;
+
+        block.dimension.spawnParticle("cosmos:chamber_up", {x: x + 0.3 + Math.random() * 0.4, y: y, z: z + 0.3 + Math.random() * 0.4})
+        block.dimension.spawnParticle("cosmos:chamber_up", {x: x + 0.3 + Math.random() * 0.4, y: y, z: z + 0.3 + Math.random() * 0.4})
+        
+        block.dimension.spawnParticle("cosmos:chamber_down", {x: x + 0.3 + Math.random() * 0.4, y: y + 2.8, z: z + 0.3 + Math.random() * 0.4})
+        block.dimension.spawnParticle("cosmos:chamber_down", {x: x + 0.3 + Math.random() * 0.4, y: y + 2.8, z: z + 0.3 + Math.random() * 0.4})
+    },
+}
+
+function play_camera_animation(player, pos, degree, cam_degree){
+    let anim = new CatmullRomSpline();
+    anim.controlPoints = [
+        { x: pos.x + 0.5 + Math.cos((90 + degree)/57.2957795147) * 5.05, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((90 + degree)/57.2957795147) * 5.05 },
+        { x: pos.x + 0.5 + Math.cos((108 + degree)/57.2957795147) * 5, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((108 + degree)/57.2957795147) * 5 },
+        { x: pos.x + 0.5 + Math.cos((126 + degree)/57.2957795147) * 5, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((126 + degree)/57.2957795147) * 5 },
+        { x: pos.x + 0.5 + Math.cos((144 + degree)/57.2957795147) * 5, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((144 + degree)/57.2957795147) * 5 },
+        { x: pos.x + 0.5 + Math.cos((162 + degree)/57.2957795147) * 5, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((162 + degree)/57.2957795147) * 5 },
+        { x: pos.x + 0.5 + Math.cos((180 + degree)/57.2957795147) * 5, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((180 + degree)/57.2957795147) * 5 },
+        { x: pos.x + 0.5 + Math.cos((198 + degree)/57.2957795147) * 5, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((198 + degree)/57.2957795147) * 5 },
+        { x: pos.x + 0.5 + Math.cos((216 + degree)/57.2957795147) * 5, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((216 + degree)/57.2957795147) * 5 },
+        { x: pos.x + 0.5 + Math.cos((234 + degree)/57.2957795147) * 5, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((234 + degree)/57.2957795147) * 5 },
+        { x: pos.x + 0.5 + Math.cos((252 + degree)/57.2957795147) * 5, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((252 + degree)/57.2957795147) * 5 },
+        { x: pos.x + 0.5 + Math.cos((270 + degree)/57.2957795147) * 5.05, y: pos.y + 2, z: pos.z + 0.5 + Math.sin((270 + degree)/57.2957795147) * 5.05 },
+
+    ];
+
+    const progressKeyFrames = [
+        { alpha: 0, timeSeconds: 0.0 },
+        { alpha: 0.1, timeSeconds: 0.4 },
+        { alpha: 0.2, timeSeconds: 0.8 },
+        { alpha: 0.3, timeSeconds: 1.2 },
+        { alpha: 0.4, timeSeconds: 1.6 },
+        { alpha: 0.5, timeSeconds: 2 },
+        { alpha: 0.6, timeSeconds: 2.4 },
+        { alpha: 0.7, timeSeconds: 2.8 },
+        { alpha: 0.8, timeSeconds: 3.2},
+        { alpha: 0.9, timeSeconds: 3.6},
+        { alpha: 1.0, timeSeconds: 4 },
+    ];
+
+    const rotationKeyFrames = [
+        { rotation: { x: 0, y: 360 + cam_degree, z: 0 }, timeSeconds: 0.00, easingFunc: EasingType.Linear },
+        { rotation: { x: 0, y: 180 + cam_degree, z: 0 }, timeSeconds: 4, easingFunc: EasingType.Linear }
+    ];
+
+    player.camera.setCamera("minecraft:free", {location: player.location, rotation: {x: 0, y: 270}});
+    player.camera.playAnimation(anim, { totalTimeSeconds: 4.0, animation: { progressKeyFrames: progressKeyFrames, rotationKeyFrames: rotationKeyFrames }});
 }
